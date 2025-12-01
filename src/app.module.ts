19608827bf93397017d7530a4277f4008969e9e4
Andy-M7 <allataspariguana@gmail.com>
2025-11-30 21:52:56 -0500

    Configuracion inicial app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
 import { FirebaseModule } from './firebase/firebase.module';
 
 @Module({
   imports: [
+    // ⚡ Habilita .env y variables globales
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // ⚡ Base de datos
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
